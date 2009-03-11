b3bdcf292619ef99469f661a5f238e7b3b32c7e3
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-03-11 21:08:35 +0000

    New tests. New civet output collection model. Civet results displayed differently.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@158 bcc1f7e1-216c-4ce3-888e-c5a8bb225047
    
    Former-commit-id: a82eea7769951f2be52fc54dec4601a46715eb45

---
+++ BrainPortal/app/models/civet_collection.rb
@@ -0,0 +1,65 @@
+#
+# CBRAIN Project
+#
+# Civet collection model
+# Essentially a file collection with some methods for handling civet output
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+class CivetCollection < FileCollection
+  Revision_info="$Id$"
+  
+  def list_native
+    @native_list ||= self.get_list('native')
+  end
+  
+  def list_classify
+    @classify_list ||= self.get_list('classify')
+  end
+  
+  def list_final
+    @final_list ||= self.get_list('final')
+  end
+  
+  def list_logs
+    @logs_list ||= self.get_list('logs')
+  end
+  
+  def list_surfaces
+    @surfaces_list ||= self.get_list('surfaces')
+  end
+  
+  def list_temp
+    @temp_list ||= self.get_list('temp')
+  end
+  
+  def list_thickness
+    @thickness_list ||= self.get_list('thickness')
+  end
+  
+  def list_linear_transforms
+    @linear_list ||= self.get_list('transforms/linear')
+  end
+  
+  def list_non_linear_transforms
+    @non_linear_list ||= self.get_list('transforms/nonlinear')
+  end
+  
+  def list_surfreg_transforms
+    @surfreg_list ||= self.get_list('transforms/surfreg')
+  end
+  
+  def list_verify
+    @verify_list ||= self.get_list('verify')
+  end
+  
+  
+  def get_list(directory)
+    Dir.chdir(self.user.vault_dir + self.name) do
+      `find #{directory} -type f`.split("\n").map{ |name| name.sub(/^#{directory}\//, "") }
+    end
+  end
+end
