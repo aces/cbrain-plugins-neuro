d41109f8533d93905a187c2ba640eab238118383
prioux <prioux@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2009-09-09 20:52:58 +0000

    Added a whole lot of svn:ignore properties all over the tree so that the output of svn status is much cleaned up of stuff we regularly find in there but don't care about.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@484 bcc1f7e1-216c-4ce3-888e-c5a8bb225047
    
    Former-commit-id: 44e5372668456c7ce2aa49c730770e87a726ac74

--- BrainPortal/app/models/drmaa_civet.rb
+++ BrainPortal/app/models/drmaa_civet.rb
@@ -14,23 +14,25 @@ class DrmaaCivet < DrmaaTask
     file_ids = params[:file_ids]
     file_args = []
     civet_args = nil
+
+    user_id = params[:user_id]
     
     file_ids.each do |mincfile_id|
       # This block of code is executed only once in an edit
       # session, when we first enter the page
-        mincfile      = Userfile.find(mincfile_id)
-        basename      = mincfile.name
-        if basename.match(/([^\/]+)_(\w+)_t1(_\w+)?\.mnc(\.gz|.Z)?$/)
-          prefix = Regexp.last_match[1]
-          dsid   = Regexp.last_match[2]
-        else
-          prefix = "prefix"
-          dsid   = "dsid"
-        end
+      mincfile      = Userfile.find(mincfile_id)
+      basename      = mincfile.name
+      if basename.match(/([^\/]+)_(\w+)_t1(_\w+)?\.mnc(\.gz|.Z)?$/)
+        prefix = Regexp.last_match[1]
+        dsid   = Regexp.last_match[2]
+      else
+        prefix = "prefix"
+        dsid   = "dsid"
+      end
 
-        # These three Userfile IDs are for (the optional) t2, pd and mask files
-        # associated with the main t1 file.
-        (t2_id,pd_id,mk_id) = find_t2_pd_mask(basename)
+      # These three Userfile IDs are for (the optional) t2, pd and mask files
+      # associated with the main t1 file.
+      (t2_id,pd_id,mk_id) = find_t2_pd_mask(basename,user_id)
     
       file_args << {
         :mincfile_id         => mincfile_id,
@@ -116,17 +118,19 @@ class DrmaaCivet < DrmaaTask
     params[:civet_args]
   end
   
-  def self.find_t2_pd_mask(t1name) #:nodoc:
+  # TODO need ability to find these files even when they don't
+  # belong to the user but are still 'accessible' by the user's groups
+  def self.find_t2_pd_mask(t1name,user_id) #:nodoc:
       if ! t1name.match(/_t1/)
           return [nil,nil,nil]
       end
-      t2 = Userfile.find_by_name(t1name.sub(/_t1/,"_t2"))
+      t2 = Userfile.find(:first, :conditions => { :name => t1name.sub(/_t1/,"_t2"), :user_id => user_id } )
       t2_id = t2 ? t2.id : nil
 
-      pd = Userfile.find_by_name(t1name.sub(/_t1/,"_pd"))
+      pd = Userfile.find(:first, :conditions => { :name => t1name.sub(/_t1/,"_pd"), :user_id => user_id } )
       pd_id = pd ? pd.id : nil
 
-      mk = Userfile.find_by_name(t1name.sub(/_t1/,"_mask"))
+      mk = Userfile.find(:first, :conditions => { :name => t1name.sub(/_t1/,"_mask"), :user_id => user_id } )
       mk_id = mk ? mk.id : nil
 
       [t2_id,pd_id,mk_id]
