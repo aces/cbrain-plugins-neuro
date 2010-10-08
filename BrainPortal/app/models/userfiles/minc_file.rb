5a05b522cb54df81d739c2ef2c712d368abf7a06
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2010-10-08 15:43:50 +0000

    Added minc file type and jiv viewer.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1564 bcc1f7e1-216c-4ce3-888e-c5a8bb225047
    
    Former-commit-id: 5913da39641bdedcbf7298539d3949c0287a9ff6

---
+++ BrainPortal/app/models/userfiles/minc_file.rb
@@ -0,0 +1,25 @@
+#
+# CBRAIN Project
+#
+# MincFile model
+#
+# Original author: Tarek Sherif
+#
+# $Id$
+#
+
+class MincFile < SingleFile
+
+  Revision_info="$Id$"
+  
+  has_viewer :name  => "Jiv Viewer", :partial  => "jiv_file", :if  => Proc.new{ |u| u.has_format?(:jiv) && u.get_format(:jiv).is_locally_synced? }
+  
+  def format_name
+    "MINC"
+  end
+  
+  def pretty_type
+    "(Minc)"
+  end
+  
+end
