6808e8591237b92559fe112de3c9ed692310bb2b
tsherif <tsherif@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2011-03-11 17:07:56 +0000

    Fixed bug in civet study viewer.
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1863 bcc1f7e1-216c-4ce3-888e-c5a8bb225047
    
    Former-commit-id: 9592ed871fd4e73e17e4fe0d24623f28180e8a1f

--- BrainPortal/app/models/userfiles/civet_collection.rb
+++ BrainPortal/app/models/userfiles/civet_collection.rb
@@ -27,61 +27,6 @@ class CivetCollection < FileCollection
       super
     end
   end
-
-  #List files in the +native+ subdirectory.
-  def list_native
-    @native_list ||= get_full_subdir_listing('native')
-  end
-  
-  #List files in the +classify+ subdirectory.
-  def list_classify
-    @classify_list ||= get_full_subdir_listing('classify')
-  end
-  
-  #List files in the +final+ subdirectory.
-  def list_final
-    @final_list ||= get_full_subdir_listing('final')
-  end
-  
-  #List files in the +logs+ subdirectory.
-  def list_logs
-    @logs_list ||= get_full_subdir_listing('logs')
-  end
-  
-  #List files in the +surfaces+ subdirectory.
-  def list_surfaces
-    @surfaces_list ||= get_full_subdir_listing('surfaces')
-  end
-  
-  #List files in the +temp+ subdirectory.
-  def list_temp
-    @temp_list ||= get_full_subdir_listing('temp')
-  end
-  
-  #List files in the +thickness+ subdirectory.
-  def list_thickness
-    @thickness_list ||= get_full_subdir_listing('thickness')
-  end
-  
-  #List files in the +transforms+/+linear+ subdirectory.
-  def list_linear_transforms
-    @linear_list ||= get_full_subdir_listing('transforms/linear')
-  end
-  
-  #List files in the +transforms+/+nonlinear+ subdirectory.
-  def list_non_linear_transforms
-    @non_linear_list ||= get_full_subdir_listing('transforms/nonlinear')
-  end
-  
-  #List files in the +transforms+/+surfreg+ subdirectory.
-  def list_surfreg_transforms
-    @surfreg_list ||= get_full_subdir_listing('transforms/surfreg')
-  end
-  
-  #List files in the +verify+ subdirectory.
-  def list_verify
-    @verify_list ||= get_full_subdir_listing('verify')
-  end
   
   def qc_images
     self.list_files("verify").select { |f| f.name =~ /\.png$/ }
