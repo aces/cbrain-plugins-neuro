a5116e0c833f3a06468313801eaf619cf7793840
nkassis <nkassis@bcc1f7e1-216c-4ce3-888e-c5a8bb225047>
2011-04-12 17:21:52 +0000

    added braincanvas
    git-svn-id: svn+ssh://huia.bic.mni.mcgill.ca/home/cbrain/cbrain_repo/trunk@1905 bcc1f7e1-216c-4ce3-888e-c5a8bb225047
    
    Former-commit-id: f91d0d698b8d9a085733c9037ecccfcbf43254bf

--- BrainPortal/app/models/userfiles/minc_file.rb
+++ BrainPortal/app/models/userfiles/minc_file.rb
@@ -8,18 +8,32 @@
 # $Id$
 #
 
+require 'minc'
+
 class MincFile < SingleFile
 
   Revision_info="$Id$"
   
   has_viewer :partial  => "jiv_file", :if  => Proc.new{ |u| u.has_format?(:jiv) && u.get_format(:jiv).is_locally_synced? }
   
+  has_viewer :partial => "html5_minc_viewer", :if  => Proc.new{ |u| u.is_locally_synced? && u.size < 30.megabytes}
+
   def format_name
     "MINC"
   end
   
   def self.file_name_pattern
-    /\.mi?nc(\.gz|\.Z|\.gz2)?$/i
+    /\.mi?nc(\.gz|\.Z|\.gz2)?$/
   end
   
+  def content(params) 
+    minc = Minc.new(self.cache_full_path)
+    if params["minc_headers"]
+      return {:text => minc.headers.to_json}
+    elsif params["raw_data"]
+      return {:text => minc.raw_data}
+    end    
+  end
+
 end
+ 
