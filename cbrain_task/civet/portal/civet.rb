93a5b49b536356221b6ad9620bc2416ad0cf5ee5
Pierre Rioux <pierre.rioux@mcgill.ca>
2016-09-08 11:07:24 -0400

    Removed attribute 'launch_time'.

--- cbrain_task/civet/portal/civet.rb
+++ cbrain_task/civet/portal/civet.rb
@@ -569,7 +569,6 @@ class CbrainTask::Civet < PortalTask
     combiner.description      = study_name
     combiner.status           = 'New'
     combiner.group_id         = self.group_id
-    combiner.launch_time      = self.launch_time
     combiner.batch_id         = self.batch_id
     combiner.params = {
       :civet_study_name     => study_name,
@@ -595,7 +594,6 @@ class CbrainTask::Civet < PortalTask
     qc.description = params[:study_name]
     qc.status      = 'New'
     qc.group_id    = self.group_id
-    qc.launch_time = self.launch_time
     qc.batch_id    = self.batch_id
     qc.params      = { :study_from_task_id => cid }
     qc.add_prerequisites_for_setup(cid)
