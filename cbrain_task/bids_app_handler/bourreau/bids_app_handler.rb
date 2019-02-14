e3c31f9d63bde5b8239550878e972659a48b0207
Pierre Rioux <pierre.rioux@mcgill.ca>
2019-02-13 19:06:46 -0500

    Added BidsAppHandler abstract class.
    JSON Boutiques descriptors for BidsApps can be
    placed in the bids_app_descriptors.
    
    Limitation: I'm using run_id too often and it
    interferes with the restart capabilities. TODO.

---
+++ cbrain_task/bids_app_handler/bourreau/bids_app_handler.rb
@@ -0,0 +1,226 @@
+
+# A subclass of CbrainTask::ClusterTask to run BidsAppHandler.
+class CbrainTask::BidsAppHandler < ClusterTask
+
+  Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
+
+  include RestartableTask
+  include RecoverableTask
+
+  Bosh_Required_Version = "0.5.18" # 0.5.18 fails too; waiting for patches, 2019-02-13
+
+  # See the CbrainTask Programmer Guide
+  def setup #:nodoc:
+    bids_dataset = self.bids_dataset
+
+    mode = params[:_cb_mode]
+    return true if mode =~ /save|group/ # a 'save' or 'group' task doesn't need any setup
+
+    # Make sure 'bosh' is recent enough
+    return false unless self.bosh_is_supported?
+
+    addlog("Synchronizing #{bids_dataset.name}")
+    make_available(bids_dataset, bids_dataset.name)
+
+    return true
+  end
+
+  # See the CbrainTask Programmer Guide
+  def cluster_commands #:nodoc:
+    params       = self.params
+    bids_dataset = self.bids_dataset
+
+    mode         = params[:_cb_mode]
+    return "" if mode == 'save' # a 'save' task doesn't do any cluster processing
+
+    # Output goes to...
+    outputdir = output_dir_basename_for_batch()
+    # Output directory for run
+    FileUtils.mkdir_p(outputdir) # we create it here so that 'restart at cluster' work.
+
+    # Build the bosh 'invoke' JSON structure
+    invoke_json = params.dup.reject do |property,value|
+      (property =~ /^_cb_/) ||
+      ([ 'interface_userfile_ids', ].include?(property.to_s))
+    end
+
+    invoke_json.merge!({
+      :bids_dir          => bids_dataset.name,
+      :participant_label => selected_participants,
+      :analysis_level    => mode,
+    })
+    if mode == 'participant'
+      invoke_json.merge!( { :output_dir_name => outputdir, } )
+    end
+    if mode == 'group'
+      invoke_json.merge!( { :output_dir_name => outputdir, } )
+      #invoke_json.merge!( { :participant_level_analysis_dir => outputdir, } )
+    end
+
+    # Write it as a file
+    invoke_json_basename = "invoke.#{self.run_id()}.json"
+    File.open(invoke_json_basename,"w") do |fh|
+      fh.write invoke_json.to_json
+    end
+
+    # Boutique task descriptor.
+    # We need the version with all the inputs and also
+    # the container image information in it.
+    boutiques_json_basename = "boutiques-T#{self.batch_id.presence || self.id}.json"
+    unless File.exists? boutiques_json_basename
+      descriptor = self.class.full_descriptor
+      File.open("#{boutiques_json_basename}.#{Process.pid}.tmp","w") do |fh|
+        fh.write descriptor.to_json
+      end
+      File.rename "#{boutiques_json_basename}.#{Process.pid}.tmp", boutiques_json_basename
+    end
+
+    # Prepare execution (pulling container image mostly)
+    image_basename = "#{self.class}.simg"
+    cached_image = SingularityImage.find_or_create_as_scratch(:name => image_basename) do |cache_path|
+      self.addlog('Preparing container image')
+      parent  = Pathname.new(cache_path).parent
+      out,err = self.tool_config_system(
+        "cd #{parent.to_s.bash_escape} && " + # bosh 0.5.18 is dumb
+        "bosh exec prepare #{self.full_cluster_workdir.to_s.bash_escape}/#{boutiques_json_basename} && " + # very dumb
+        "mv *.simg #{cache_path.to_s.bash_escape}" # uselessly dumb
+        # REAL command once bosh is fixed:
+        # "bosh exec prepare --imagepath #{cache_path.bash_escape} #{boutiques_json_basename}"
+      )
+      cb_error("Cannot prepare singularity image? Bosh output=\n#{out}#{err}") unless File.exists?(cache_path.to_s)
+    end
+    self.addlog("Container image prepared: \"#{cached_image.name}\" (ID=#{cached_image.id})")
+
+    # Prepare mount paths for local data providers.
+    # This will be a string "-v path1:path1 -v path2:path2 -v path3:path3"
+    # just like bosh expects.
+    # local_dp_storage_paths() is a private method of ClusterTask.
+    esc_local_dp_mountpoints = local_dp_storage_paths.inject("") do |sing_opts,path|
+      "#{sing_opts} -v #{path.bash_escape}:#{path.bash_escape}"
+    end
+
+    # The bosh launch command. This is all a single line, but broken up
+    # for readability.
+    commands = <<-COMMANDS
+      mkdir -p status.all
+      rm -f status.all/#{run_id}
+      bosh exec launch                                                \
+        --imagepath #{cached_image.cache_full_path.to_s.bash_escape}  \
+        #{esc_local_dp_mountpoints}                                   \
+        #{boutiques_json_basename.bash_escape}                        \
+        #{invoke_json_basename.bash_escape}
+      echo $? > status.all/#{run_id}
+    COMMANDS
+
+    [ commands ]
+  end
+
+  # See the CbrainTask Programmer Guide
+  def save_results #:nodoc:
+    params       = self.params
+    bids_dataset = self.bids_dataset
+    mode         = params[:_cb_mode]
+
+    # Check that we have a zero status code from bosh
+    if mode != 'save' # a 'save' task doesn't produce a status file
+      status = File.read("status.all/#{run_id}").strip rescue 'No status file'
+      if status != '0'
+        self.addlog("Bosh did not return a success status. Got: #{status}")
+        return false
+      end
+    end
+
+    # Check that we have at least one output file per participant
+    outputdir            = output_dir_basename_for_batch()
+    glob                 = Dir.glob "#{outputdir}/*"
+    all_ok               = true
+    participants_outputs = {}
+    selected_participants.each do |sub|
+      sublist = glob.select { |p| Pathname.new(p).basename.to_s =~ /^sub-#{Regexp.quote(sub)}(\b|_)/ }
+      participants_outputs[sub] = sublist
+      if sublist.present?
+        self.addlog "Found #{sublist.size} output files for participant '#{sub}'"
+      else
+        self.addlog "Error: can't find outputs for participant '#{sub}'"
+        all_ok = false
+      end
+    end
+
+    # We don't do any saving for participants tasks
+    if mode == 'participant'
+      self.addlog("No output files need saving here, a separate task handles that.")
+      return all_ok
+    end
+
+    # For provevance logs at the end
+    created_outputs = []
+
+    # For mode 'save', we just save each participant's result
+    if mode == 'save'
+      selected_participants.each do |sub|
+        sublist = participants_outputs[sub]
+        sublist.each do |one_output| # "outdir/something"
+          out_type = File.file?(one_output) ? SingleFile : FileCollection
+          basename = Pathname.new(one_output).basename.to_s #hopefully respects CBRAIN legal characters
+          cb_out = safe_userfile_find_or_new(out_type, { :name => basename + "_" + run_id() })
+          cb_out.cache_copy_from_local_file(one_output)
+          cb_out.move_to_child_of( bids_dataset )
+          created_outputs << cb_out
+        end
+      end
+    end
+
+    # For mode 'group', we just save the output dir containing all participants outputs
+    if mode == 'group'
+      group_output_name = 'group_output' + "_" + run_id()
+      self.addlog("Attempting to save group results '#{group_output_name}'")
+      cb_out = safe_userfile_find_or_new(FileCollection, { :name => group_output_name })
+      cb_out.cache_copy_from_local_file(outputdir)
+      cb_out.move_to_child_of(bids_dataset)
+      created_outputs << cb_out
+    end
+
+    # Add provenance logs
+    self.addlog_to_userfiles_these_created_these( bids_dataset, created_outputs )
+
+    return true
+  end
+
+  protected
+
+  def output_dir_basename_for_batch #:nodoc:
+    batch_id = self.batch_id || self.id
+    "outdir-#{batch_id}-#{run_number}"
+  end
+
+  def bosh_is_supported? #:nodoc:
+    out, err = self.tool_config_system("bosh version")
+
+    if err.present?
+      self.addlog("No proper 'bosh' program detected on system. Got stderr:")
+      self.addlog(err)
+      return false
+    end
+
+    if out.blank?
+      self.addlog("No proper 'bosh' program detected on system. Got no outputs")
+      return false
+    end
+
+    if out !~ /(\d+\.\d+\.\d+)$/ # not \z here
+      self.addlog("Program 'bosh' did not return a version number a.b.c. Got:")
+      self.addlog(out)
+      return false
+    end
+
+    bosh_version = Regexp.last_match[1]
+    if ToolConfig.compare_versions(bosh_version, Bosh_Required_Version) < 0
+      self.addlog("Program 'bosh' is too old. Got version #{bosh_version}, expected at least #{Bosh_Required_Version}")
+      return false
+    end
+
+    true
+  end
+
+end
+
---
+++ cbrain_task/bids_app_handler/common/bids_app_handler.rb
@@ -0,0 +1,172 @@
+
+# Model code common to the Bourreau and Portal side for BidsAppHandler.
+class CbrainTask::BidsAppHandler
+
+  Common_Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
+
+  HandledBoutiquesInputs  = %w( bids_dir analysis_level participant_label output_dir_name )
+  HandledBoutiquesOutputs = %w( output_dir )
+
+  # Two variants of the descriptor are stored in the built subclasses:
+
+  # 1) The method 'generated_from' (provided by the Boutiques integrator) returns the
+  # descriptor without BIDs inputs and without container info.
+
+  # 2) This method returns the full descriptor
+  class_attribute :full_descriptor
+
+  # Automatically register the task's version when new() is invoked.
+  def initialize(arguments = {}) #:nodoc:
+    super(arguments)
+    baserev = Revision_info # will come from Portal or Bourreau side
+    self.addlog("Base #{baserev.basename} rev. #{baserev.short_commit}", :caller_level => 2)
+    commonrev = Common_Revision_info
+    self.addlog("Common #{commonrev.basename} rev. #{commonrev.short_commit}", :caller_level => 2)
+  end
+
+  # Returns the single bids_dataset that we originally
+  # selected from the form (first ID in params[:interface_userfile_ids])
+  # Raise an exception if it's not right.
+  def bids_dataset #:nodoc:
+    return @bids_dataset if @bids_dataset
+    ids    = params[:interface_userfile_ids] || []
+    bid    = ids[0] || -1
+    @bids_dataset = BidsDataset.where(:id => bid).first
+    cb_error "This task requires a single BidDataset as input" unless @bids_dataset.present?
+    @bids_dataset
+  end
+
+  # Returns the participants that were selected by their checkboxes in the launch form
+  def selected_participants #:nodoc:
+    select_hash  = params[:_cb_participants] || {}
+    select_hash.keys.select { |sub| select_hash[sub] == '1' }
+  end
+
+  ###########################################################################
+  # Boot-time loader methods
+  #
+  # (This could probably be put elsewhere)
+  ###########################################################################
+
+  def self.boutiques_json_loader(json_file) #:nodoc:
+    bids_app_full_json = SchemaTaskGenerator.expand_json(json_file)
+    self.validate_bids_app_json(bids_app_full_json)
+
+    # Step 1, remove container info
+    without_container = self.json_without_container_config(bids_app_full_json)
+
+    # Step 2, remove BidsApp inputs that we handle ourselves
+    partial_bids_app_json = self.json_without_bids_app_params(without_container)
+
+    # Step 3, add superclass
+    partial_bids_app_json["custom"] ||= {}
+    partial_bids_app_json["custom"]['cbrain:inherits-from-class'] = self.name  # The generator will use self as superclass
+
+    # OK, so this is the standard Boutiques integration, minus some
+    # input and output params and with a new superclass
+    schema = SchemaTaskGenerator.default_schema
+    generated = SchemaTaskGenerator.generate(schema, partial_bids_app_json, true, json_file)
+    subclass  = generated.integrate # this will be a subclass of self
+
+    # Make adjustments to view rendering code (wrappers)
+    subclass.adjust_boutiques_raw_partial
+
+    # Store JSON original descriptor in our new subclass (using class_attribute)
+    subclass.full_descriptor = bids_app_full_json
+
+    subclass
+  end
+
+  def self.validate_bids_app_json(descriptor) #:nodoc:
+    inputs = descriptor['inputs'] || []
+    HandledBoutiquesInputs.each do |id|
+      raise "Descriptor missing '#{id}' in inputs." unless
+        inputs.any? { |struct| struct['id'] == id }
+    end
+    outputs = descriptor['output-files'] || []
+    HandledBoutiquesOutputs.each do |id|
+      raise "Descriptor missing '#{id}' in outputs." unless
+        outputs.any? { |struct| struct['id'] == id }
+    end
+    containerinfo = descriptor['container-image']
+    raise 'Descriptor missing singularity container info.' if containerinfo.blank?
+    cont_type = containerinfo['type']
+    raise "Descriptor has container type of #{cont_type} instead of singularity" if cont_type != 'singularity'
+  end
+
+  def self.json_without_bids_app_params(descriptor) #:nodoc:
+    cleaned = descriptor.dup
+
+    # Remove the inputs that we handle ourselves
+    cleaned['inputs'] = cleaned['inputs'].reject do |struct|
+      HandledBoutiquesInputs.include? struct['id']
+    end
+
+    #cleaned['output-files'] = cleaned['output-files'].reject do |struct|
+    #  HandledBoutiquesOutputs.include? struct['id']
+    #end
+    #cleaned.delete('output-files') if cleaned['output-files'].empty?
+
+    # If the Boutiques dev configured a mutex grouping, we remove it too.
+    if cleaned['groups'].present?
+      cleaned['groups'] = cleaned['groups'].reject do |struct|
+        members = struct['members'] || []
+        members.sort ==  [ "output_dir_name", "participant_level_analysis_dir" ]
+      end
+      cleaned.delete('groups') if cleaned['groups'].empty? # spec says if it exists, it must include one entry
+    end
+
+    # Return a simplified description for the generator
+    cleaned
+  end
+
+  def self.json_without_container_config(descriptor) #:nodoc:
+    adjusted = descriptor.dup
+    adjusted.delete('container-image')
+    adjusted
+  end
+
+  def self.adjust_boutiques_raw_partial #:nodoc:
+    generated = self.generated_from
+    handler_view_dir = File.expand_path('../../views', __FILE__)
+
+    dispatch_hash = {
+      :orig_task_params => generated.source[:task_params],
+      :orig_show_params => generated.source[:show_params],
+      :edit_help        => generated.source[:edit_help]
+    }
+    %w( task_params show_params ).each do |myview|
+      wrapper_view = File.read("#{handler_view_dir}/_#{myview}.html.erb") # our wrappers
+      dispatch_hash[myview.to_sym] = wrapper_view
+    end
+
+    # We crush the Boutiques integrator raw_partial() method with our new one.
+    self.define_singleton_method(:raw_partial) do |partial|
+      dispatch_hash[partial]
+    end
+  end
+
+end
+
+#############################################################
+# Pre-load all BidsApp JSON
+#############################################################
+
+1.times do
+  bids_app_json_dir = File.expand_path('../../bids_app_descriptors', __FILE__)
+  Dir
+    .entries(bids_app_json_dir)
+    .select { |x| x =~ /\.json$/ }
+    .sort
+    .each do |json_file|
+      #puts "\e[31mDEBUG: Trying to integrate BidsApps JSON '#{json_file}'...\e[0m"
+      full_path = Pathname.new(bids_app_json_dir) + json_file
+      begin
+        subclass = CbrainTask::BidsAppHandler.boutiques_json_loader( full_path.to_s )
+        puts "C> #{subclass.superclass}: Integrated #{subclass}"
+      rescue => ex
+        puts "C> Skipping integrating BidsApp descriptor '#{json_file}': #{ex.class} #{ex.message}"
+      end
+    end
+end
+
