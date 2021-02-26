ad452a6d68821d1fb932215dc2be645920bbc194
Natacha Beck <natabeck@gmail.com>
2021-02-26 10:08:04 -0500

    [RM] useless function in MincFile

--- userfiles/minc_file/minc_file.rb
+++ userfiles/minc_file/minc_file.rb
@@ -26,23 +26,26 @@ class MincFile < SingleFile
   Revision_info=CbrainFileRevision[__FILE__] #:nodoc:
 
   has_viewer :name => "Volume Viewer",  :partial => :volume_viewer,           :if =>
-             Proc.new { |u| u.size.present? &&
+             Proc.new { |u,size|
+                            false if !(u.size.present? || size.present?);
+                            size = size.present? ? size.to_i : u.size;
+                            u.size.present? &&
                             u.size < 400.megabytes &&
-                            u.is_locally_synced? &&
-                            ( ! u.is_a?(MincFile) || u.class.has_minctools?([2,0,0]))
+                            u.is_locally_synced?
                       }
 
   has_viewer :name => "Info & Headers", :partial => :info_header,             :if =>
              Proc.new { |u| u.class.has_minctools?([2,0,0],["mincinfo","mincheader","mincdump","mincexpand"]) && u.is_locally_synced? }
 
   has_viewer :name => "MincNavigator",  :partial => :minc_navigator,          :if =>
-             Proc.new { |u| u.size.present? &&
+             Proc.new { |u,size|
+                            false if !(u.size.present? || size.present?);
+                            size = size.present? ? size.to_i : u.size;
+                            u.size.present? &&
                             u.size < 400.megabytes &&
                             u.is_locally_synced?
                       }
 
-  has_content :method => :get_headers_to_json, :type => :text
-  has_content :method => :get_raw_data,        :type => :text
   has_content :method => :minc_content,        :type => :text
 
   def self.file_name_pattern #:nodoc:
@@ -64,72 +67,6 @@ class MincFile < SingleFile
     else
       false
     end
-  end
-
-  def minc_get_headers #:nodoc:
-
-    return @headers if @headers
-    cb_error "Call to minc_get_headers() when minctools not installed!" unless self.class.has_minctools? [2,0,0]
-
-    cache_path   = self.cache_full_path
-    escaped_path = cache_path.to_s.bash_escape
-
-    order = IO.popen("mincinfo -attval image:dimorder #{escaped_path}") {|fh| fh.readlines.join.strip.split(',')}
-    if !(order.size == 3 or order.size == 4)
-      order = IO.popen("mincinfo -dimnames #{escaped_path}") {|fh| fh.readlines.join.strip.split(' ')}
-    end
-
-    #Gets the attributes in the minc file that we need
-    @headers = {
-      :xspace => {
-        :start        => IO.popen("mincinfo -attval    xspace:start #{escaped_path}") { |fh| fh.readlines.join.to_f },
-        :space_length => IO.popen("mincinfo -dimlength xspace       #{escaped_path}") { |fh| fh.readlines.join.to_i },
-        :step         => IO.popen("mincinfo -attval    xspace:step  #{escaped_path}") { |fh| fh.readlines.join.to_f }
-      },
-      :yspace => {
-        :start        => IO.popen("mincinfo -attval    yspace:start #{escaped_path}") { |fh| fh.readlines.join.to_f },
-        :space_length => IO.popen("mincinfo -dimlength yspace       #{escaped_path}") { |fh| fh.readlines.join.to_i },
-        :step         => IO.popen("mincinfo -attval    yspace:step  #{escaped_path}") { |fh| fh.readlines.join.to_f }
-      },
-      :zspace => {
-        :start        => IO.popen("mincinfo -attval    zspace:start #{escaped_path}") { |fh| fh.readlines.join.to_f },
-        :space_length => IO.popen("mincinfo -dimlength zspace       #{escaped_path}") { |fh| fh.readlines.join.to_i },
-        :step         => IO.popen("mincinfo -attval    zspace:step  #{escaped_path}") { |fh| fh.readlines.join.to_f }
-      },
-
-      :order => order
-    }
-
-    if order.length == 4
-      @headers[:time] = {
-        :start        => IO.popen("mincinfo -attval    time:start   #{escaped_path}") { |fh| fh.readlines.join.to_f },
-        :space_length => IO.popen("mincinfo -dimlength time         #{escaped_path}") { |fh| fh.readlines.join.to_i }
-      }
-    end
-
-    @headers
-  end
-
-  #For content
-  def get_headers_to_json
-    minc_get_headers.to_json
-  end
-
-  # The raw binary data, in short integers
-  def get_raw_data #:nodoc:
-    cb_error "Call to raw_data() when minctools not installed!" unless self.class.has_minctools? [2,0,0]
-    return @raw_data if @raw_data
-    cache_path   = self.cache_full_path
-    escaped_path = cache_path.to_s.bash_escape
-    @raw_data = IO.popen("minctoraw -byte -unsigned -normalize #{escaped_path}") { |fh| fh.readlines.join }
-  end
-
-  def minc_get_data #:nodoc:
-    @data ||= self.raw_data.unpack('v*') #Unpack is used here to convert 4 byte(char) to a short unsigned integer
-  end
-
-  def minc_get_data_string #:nodoc:
-    @data_string ||= self.data.join(" ") #making a space delimited array of the data (useful to send to server)
   end
 
   #This method return the version of MINC file ('MINC1' or 'MINC2').
