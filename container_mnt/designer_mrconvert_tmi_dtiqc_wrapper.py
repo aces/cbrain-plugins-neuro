692f9c14d00f4144eabdb946a41f0f6e10338b68
Natacha Beck <777590+natacha-beck@users.noreply.github.com>
2025-11-25 16:44:58 -0500

    [ADD] dtiQC to designer_mrconvert_tmi pipeline

--- container_mnt/designer_mrconvert_tmi_dtiqc_wrapper.py
+++ container_mnt/designer_mrconvert_tmi_dtiqc_wrapper.py
@@ -12,10 +12,18 @@ import glob
 import os
 import code
 
+##########################################
+# Genaral utility functions
+##########################################
 
 def print_log(message):
     time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"{time_stamp}: {message}", flush=True)
+
+def print_header(version):
+    print_log(f"Starting designer, mrconvert, and tmi wrapper script")
+    print_log(f"By Natacha Beck nbeck@mcin.ca, based on code from Alex Pastor Bernier")
+    print_log(f"Version: {version}")
 
 def run_command(cmd, description,):
     print_log(f"Running command: {' '.join(cmd)}")
@@ -26,6 +34,73 @@ def run_command(cmd, description,):
         print_log(f"Error running {description}: {error}, code: {error.returncode}")
         print_log(f"Command: {cmd}")
         sys.exit(error.returncode)
+
+def parse_args_list():
+    parser = argparse.ArgumentParser(description='This is a wrapper to run designer, mrconvert, and tmi commands')
+
+    # Bids directory input
+    parser.add_argument('bids_dir',                                 type=str, help='BIDS directory input')
+    parser.add_argument('output',                                   type=str, help='Designer output directory')
+
+    # Inputs/Outputs parameters
+    parser.add_argument('--designer_bids_subject',  required=True,  type=str, help='Designer: BIDS subject name')
+    parser.add_argument('--designer_ses_pattern',                   type=str, help='Designer: session pattern')
+    parser.add_argument('--designer_file_pattern',  required=True,  type=str, help='Designer: file pattern')
+
+    # mrtrix parameters
+    parser.add_argument('--mrtrix_BZeroThreshold',  type=float,          help='mrtrix: BZeroThreshold value')
+
+    # Designer parameters
+    parser.add_argument('--designer_eddy',          action='store_true', help='Designer: eddy flag')
+    parser.add_argument('--designer_denoise',       action='store_true', help='Designer: denoise flag')
+    parser.add_argument('--designer_shrinkage',     type=str,            help='Designer: specify shrinkage type for MPPCA')
+    parser.add_argument('--designer_algorithm',     type=str,            help='Designer: algorithm string')
+    parser.add_argument('--designer_degibbs',       action='store_true', help='Designer: degibbs flag')
+    parser.add_argument('--designer_pf',            type=float,          help='Designer: pf number')
+    parser.add_argument('--designer_pe_dir',        type=str,            help='Designer: pe_dir string')
+    parser.add_argument('--designer_b1correct',     action='store_true', help='Designer: b1correct flag')
+    parser.add_argument('--designer_normalize',     action='store_true', help='Designer: normalize flag')
+    parser.add_argument('--designer_scratch',       type=str,            help='Designer: scratch string')
+    parser.add_argument('--designer_nocleanup',     action='store_true', help='Designer: nocleanup flag')
+
+    # mrconvert parameters
+    parser.add_argument('--mrconvert_fslgrad',      action='store_true', help='mrconvert: fslgrad flag')
+
+    # tmi parameters
+    parser.add_argument('--tmi_DKI',                action='store_true', help='tmi: DKI flag')
+    parser.add_argument('--tmi_DTI',                action='store_true', help='tmi: DTI flag')
+    parser.add_argument('--tmi_nocleanup',          action='store_true', help='tmi: nocleanup flag')
+
+    args = parser.parse_args()
+
+    return args
+
+def add_extra_args(args):
+    # Get absolute path of output directory
+    output_absolute_path = os.path.abspath(args.output)
+    args.output_absolute_path = output_absolute_path
+    # tmi output phase directory
+    args.tmi_output_phase_dir = f"{output_absolute_path}/tmi_output_phase"
+
+##########################################
+# Prepare commands
+###########################################
+
+# Mrtrix configuration
+######################
+
+def prepare_mtrix_conf(args):
+    # Create .mrtrix.conf file
+    mrtrix_conf_path = os.path.expanduser("~/.mrtrix.conf")
+    if not os.path.exists(mrtrix_conf_path):
+        with open(mrtrix_conf_path, 'w') as conf_file:
+            conf_file.write(f"BZeroThreshold: {args.mrtrix_BZeroThreshold}\n")
+    print_log(f"Created mrtrix configuration file at {mrtrix_conf_path}")
+    os.environ['MRTRIX_CONFIGFILE'] = mrtrix_conf_path
+    print_log(f"Set MRTRIX_CONFIGFILE environment variable to {mrtrix_conf_path}")
+
+# Designer preparation
+######################
 
 def designer_file_extraction(args):
 
@@ -93,59 +168,15 @@ def designer_file_extraction(args):
 
     return magnitudes, phases, rpe_pair
 
-def main():
-    os.environ['PYTHONUNBUFFERED'] = '1'
-    version = "1.0.0"
-    print_log(f"Starting designer, mrconvert, and tmi wrapper script")
-    print_log(f"By Natacha Beck nbeck@mcin.ca, based on code from Alex Pastor Bernier")
-    print_log(f"Version: {version}")
-
-    parser = argparse.ArgumentParser(description='This is a wrapper to run designer, mrconvert, and tmi commands')
-
-    # Bids directory input
-    parser.add_argument('bids_dir',                                 type=str, help='BIDS directory input')
-    parser.add_argument('output',                                   type=str, help='Designer output directory')
-
-    # Inputs/Outputs parameters
-    parser.add_argument('--designer_bids_subject',  required=True,  type=str, help='Designer: BIDS subject name')
-    parser.add_argument('--designer_ses_pattern',                   type=str, help='Designer: session pattern')
-    parser.add_argument('--designer_file_pattern',  required=True,  type=str, help='Designer: file pattern')
-
-    # Designer parameters
-    parser.add_argument('--designer_eddy',          action='store_true', help='Designer: eddy flag')
-    parser.add_argument('--designer_denoise',       action='store_true', help='Designer: denoise flag')
-    parser.add_argument('--designer_shrinkage',     type=str,            help='Designer: specify shrinkage type for MPPCA')
-    parser.add_argument('--designer_algorithm',     type=str,            help='Designer: algorithm string')
-    parser.add_argument('--designer_degibbs',       action='store_true', help='Designer: degibbs flag')
-    parser.add_argument('--designer_pf',            type=float,          help='Designer: pf number')
-    parser.add_argument('--designer_pe_dir',        type=str,            help='Designer: pe_dir string')
-    parser.add_argument('--designer_b1correct',     action='store_true', help='Designer: b1correct flag')
-    parser.add_argument('--designer_normalize',     action='store_true', help='Designer: normalize flag')
-    parser.add_argument('--designer_scratch',       type=str,            help='Designer: scratch string')
-    parser.add_argument('--designer_nocleanup',     action='store_true', help='Designer: nocleanup flag')
-
-    # mrconvert parameters
-    parser.add_argument('--mrconvert_fslgrad',      action='store_true', help='mrconvert: fslgrad flag')
-
-    # tmi parameters
-    parser.add_argument('--tmi_DKI',                action='store_true', help='tmi: DKI flag')
-    parser.add_argument('--tmi_DTI',                action='store_true', help='tmi: DTI flag')
-    parser.add_argument('--tmi_nocleanup',          action='store_true', help='tmi: nocleanup flag')
-
-    args = parser.parse_args()
-
-    cwd  = os.getcwd()
-
-
+def prepare_designer_cmd(args):
     # designer - handle extraction of magnitude and phase files
     magnitudes, phases, rpe_pair = designer_file_extraction(args)
 
     # Create output directory if it doesn't exist
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
+    if not os.path.exists(args.output_absolute_path):
+        os.makedirs(args.output_absolute_path)
 
-    # Get absolute path of output directory
-    output_absolute_path = os.path.abspath(args.output)
+
 
     # designer command construction
     designer_cmd = ["designer"]
@@ -207,41 +238,80 @@ def main():
     # Input output
     designer_cmd.append(",".join(magnitudes))
 
-    designer_cmd.append(f"{output_absolute_path}/DWI_designer.nii")
+    designer_cmd.append(f"{args.output_absolute_path}/DWI_designer.nii")
 
-    # Create .mrtrix.conf file
-    mrtrix_conf_path = Path.home() / ".mrtrix.conf"
-    if not os.path.exists(mrtrix_conf_path):
-        with open(mrtrix_conf_path, 'w') as conf_file:
-            conf_file.write("BZeroThreshold: 61\n")
+    return designer_cmd
 
-    run_command(designer_cmd, "designer")
-
-    # mrconvert
+# mrconvert preparation
+#######################
+def prepare_mrconvert_cmd(args):
     mrconvert_cmd = ["mrconvert"]
     if args.mrconvert_fslgrad:
         mrconvert_cmd.append("-fslgrad")
 
-    mrconvert_cmd.append(f"{output_absolute_path}/DWI_designer.bvec")
-    mrconvert_cmd.append(f"{output_absolute_path}/DWI_designer.bval")
-    mrconvert_cmd.append(f"{output_absolute_path}/DWI_designer.nii")
-    mrconvert_cmd.append(f"{output_absolute_path}/DWI_designer.mif")
+    mrconvert_cmd.append(f"{args.output_absolute_path}/DWI_designer.bvec")
+    mrconvert_cmd.append(f"{args.output_absolute_path}/DWI_designer.bval")
+    mrconvert_cmd.append(f"{args.output_absolute_path}/DWI_designer.nii")
+    mrconvert_cmd.append(f"{args.output_absolute_path}/DWI_designer.mif")
 
-    run_command(mrconvert_cmd, "mrconvert")
+    return mrconvert_cmd
 
-    # tmi
+# tmi preparation
+#################
+
+def prepare_tmi_cmd(args):
     tmi_cmd = ["tmi"]
     if args.tmi_DKI:
         tmi_cmd.append("-DKI")
     if args.tmi_DTI:
         tmi_cmd.append("-DTI")
+    tmi_cmd.append(args.output_absolute_path)
     if args.tmi_nocleanup:
         tmi_cmd.append("-nocleanup")
 
     tmi_cmd.append(f"{output_absolute_path}/DWI_designer.mif")
-    tmi_cmd.append(f"{output_absolute_path}/tmi_output_phase")
-    
-    run_command(tmi_cmd, "tmi")
+    tmi_cmd.append(f"{args.tmi_output_phase_dir}")
+
+    return tmi_cmd
+
+# dtiQC preparation
+###################
+def prepare_dtiqc_cmd(args):
+    dtiqc_cmd = ["bash", "/wrapper/dtiQC.sh"]
+    dtiqc_cmd.append("-i")
+    dtiqc_cmd.append(f"{args.designer_bids_subject}")
+
+    dtiqc_cmd.append("-o")
+    dtiqc_cmd.append(f"{args.tmi_output_phase_dir}")
+
+    return dtiqc_cmd
+
+def main():
+    os.environ['PYTHONUNBUFFERED'] = '1'
+    version = "1.0.1"
+    print_header(version)
+
+    args = parse_args_list()
+    add_extra_args(args)
+
+    # Mrtrix configuration
+    prepare_mtrix_conf(args)
+
+    # Designer
+    designer_cmd = prepare_designer_cmd(args)
+    run_command(designer_cmd, "designer")
+
+    # Mrconvert
+    mrconvert_cmd = prepare_mrconvert_cmd(args)
+    run_command(mrconvert_cmd, "mrconvert")
+
+    # Tmi
+    tmi_cmd = prepare_tmi_cmd(args)
+    run_command(tmi_cmd   , "tmi")
+
+    # dtiQC
+    dtiqc_cmd = prepare_dtiqc_cmd(args)
+    run_command(dtiqc_cmd, "dtiQC")
 
     print_log("Designer, mrconvert, and tmi commands completed successfully!")
 
---
+++ container_mnt/dtiQC.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+
+while getopts ":hq:i:o:g:" option; do
+   case $option in
+      h) # display Help
+      echo "Usage: wrap_defaceV2.sh
+
+### DTI_QC wraper pipeline ###########
+Based on Alex_PastorBernier_25 - McGill MNI-BIC adaptation for CBRAIN by Natacha Beck
+
+### Syntax:
+./wrap_dtiQC.sh [-i|-o|-g]
+
+Mandatory arguments
+1) -i Subject ID (sub-XXXXX)
+2) -o Output directory for QA -> DesignerV2 tmi folder with structure: tmi_output_phase
+########################################"
+
+      exit 0
+      ;;
+      i) # input
+      SID="${OPTARG}"
+      ;;
+      o) # output
+      OUT=${OPTARG}
+      ;;
+      :)                                    # If expected argument omitted:
+        echo "Error: -${OPTARG} requires an argument."
+        exit_abnormal                       # Exit abnormally.
+      ;;
+      *)                                    # If unknown (any other) option:
+        exit_abnormal                       # Exit abnormally.
+      ;;
+   esac
+done
+
+echo "Processing subject" $SID "on" $OUT
+
+# resize fa to mni res isometric
+# run the command
+cmd="resample_image --reference /cerebra/BrainExtractionBrain_mni_icbm152_t1_tal_nlin_asym_09c.nii.gz --nosmooth ${OUT}/fa_dti.nii ${OUT}/fa_dti.nii.gz"
+echo $cmd
+resample_image --reference /cerebra/BrainExtractionBrain_mni_icbm152_t1_tal_nlin_asym_09c.nii.gz --nosmooth ${OUT}/fa_dti.nii ${OUT}/fa_dti_RSZ.nii.gz
+
+#register directly fa to mni space = same space as label atlas
+antsRegistrationSyN.sh -d 3 -f /cerebra/BrainExtractionBrain_mni_icbm152_t1_tal_nlin_asym_09c.nii.gz -m ${OUT}/fa_dti_RSZ.nii.gz -o ${OUT}/fa_MNI
+
+#copy files to output
+cp /cerebra/CerebrAS_plus2RSZ.nii.gz ${OUT}/
+cp /cerebra/Cerebra_Labels_Hem.csv ${OUT}/
+
+
+#!#####################################################
+
+FOUT=${OUT}/${SID}_Atlas_proj_FA_DESIGNER_MPPCA_PHASE_JESP_Thr.csv
+echo 'DM',$(seq -f "atlas_roi%1g.nii.gz" -s ", " 1 103 | tr -s '[:blank:]' | paste -s -d,)>  ${FOUT}
+
+thr=0.9
+for file in ${OUT}/fa_MNIWarped.nii.gz; do
+
+  #returns integer total voxels and total vol in mm3, we are getting voxel ratio
+	vox_abovethr=$(fslstats -K /cerebra/CerebrAS_plus2RSZ.nii.gz ${file} -l ${thr} -V)
+
+	vox_all=$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -V)
+
+
+	# convert to array take 1st arg voxels
+  VOXALL=( $(echo "$vox_all" | cut -d ' ' -f 1) )
+  VOXABV=( $(echo "$vox_abovethr" | cut -d ' ' -f 1) )
+
+  # below give error - div by zero
+  for i in "${!VOXABV[@]}"; do
+
+    if [ ${VOXALL[$i]} == "0" ]; then # volume on this tag is zero
+    result="NA"
+    else
+    result=$(echo "scale=6; ${VOXABV[$i]} / ${VOXALL[$i]}" | bc)
+    fi
+    results+=("$result")
+
+  done
+
+  echo "Voxel_noise-ratio",$(echo ${results[@]} | tr -s '[:blank:]' ',')
+
+  minmax=$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -R)
+  max=$(echo "$minmax" | cut -d ' ' -f 2 |  tr -d '[:blank:]' | paste -s -d,)
+
+  echo "max_noise",${max}
+
+  echo "non-zero_mean_noise",$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -M | tr -s '[:blank:]' ',')
+  echo "non-zero_std",$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -S | tr -s '[:blank:]' ',')
+
+done >> ${FOUT}
+
+unset result results
+
+## Now gonna add fields concerning threshold estimates in roi 103 (genu corpus callossum)
+# open csv again 		}'
+
+thr=$(sed -n '4p' ${FOUT} | awk -F',' '{print $104}') # YES MEAN NOISE IN CC 0.83
+unset VOXALL VOXABV result results
+for file in ${OUT}/fa_MNIWarped.nii.gz; do
+
+  #returns integer total voxels and total vol in mm3, we are getting voxel ratio
+	vox_abovethr=$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -l ${thr} -V)
+	vox_all=$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -V)
+
+	# convert to array take 1st arg voxels
+  VOXALL=( $(echo "$vox_all" | cut -d ' ' -f 1) )
+  VOXABV=( $(echo "$vox_abovethr" | cut -d ' ' -f 1) )
+
+  for i in "${!VOXABV[@]}"; do
+
+    if [ ${VOXALL[$i]} == "0" ]; then # volume on this tag is zero
+    result="NA"
+    else
+    result=$(echo "scale=6; ${VOXABV[$i]} / ${VOXALL[$i]}" | bc)
+    fi
+    results+=("$result")
+  done
+
+  echo "Voxel_noise-ratio_refCC",$(echo ${results[@]} | tr -s '[:blank:]' ',')
+
+  # convert to array take 1st arg voxels
+  VOXALL=( $(echo "$vox_all" | cut -d ' ' -f 2) )
+  VOXABV=( $(echo "$vox_abovethr" | cut -d ' ' -f 2) )
+
+  minmax=$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -R)
+  max=$(echo "$minmax" | cut -d ' ' -f 2 |  tr -d '[:blank:]' | paste -s -d,)
+
+  echo "max_noise_refCC",${max}
+
+  echo "non-zero_mean_noise_refCC",$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -M | tr -s '[:blank:]' ',')
+  echo "non-zero_std_refCC",$(fslstats -K ${OUT}/CerebrAS_plus2RSZ.nii.gz ${file} -S | tr -s '[:blank:]' ',')
+
+done >> ${FOUT}
