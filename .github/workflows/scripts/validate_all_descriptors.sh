978bbb0d5e0f39b32d6bda959a835a77787f8c05
Pierre Rioux <pierre.rioux@mcgill.ca>
2026-02-03 13:30:24 -0500

    Adjusted GitHub workflow

--- .github/workflows/scripts/validate_all_descriptors.sh
+++ .github/workflows/scripts/validate_all_descriptors.sh
@@ -3,31 +3,41 @@
 # Check all descriptors using both 'jq' and 'bosh validate'
 
 cd boutiques_descriptors || exit 2
-maxerror=0
-for json in *.json ; do
-  echo "Checking $json"
 
-  jq < $json >/dev/null
+max_status=0
+
+for json in *.json ; do
+  echo ""
+  echo "--------------------------------------------"
+  echo "Checking $json"
+  echo "--------------------------------------------"
+
+  jq '.["name"]' < $json # print the name
   jq_status=$?
 
-  bosh validate $json
+  bosh validate $json  # prints "OK"
   bosh_status=$?
 
   if test $jq_status -eq 0 -a $bosh_status -eq 0 ; then
     # echo " -> passed"
     continue
-  else
-    echo " -> failed: JQ status=$jq_status ; BOSH status=$bosh_status"
   fi
 
-  if test $jq_status -gt $maxerror ; then
-    maxerror=$jq_status
+  echo " -> failed: JQ status=$jq_status ; BOSH status=$bosh_status"
+
+  if test $jq_status -gt $max_status ; then
+    max_status=$jq_status
   fi
 
-  if test $bosh_status -gt $maxerror ; then
-    maxerror=$bosh_status
+  if test $bosh_status -gt $max_status ; then
+    max_status=$bosh_status
   fi
 
 done
 
-exit $maxerror
+echo ""
+echo "--------------------------------------------"
+echo "Final exit code: $max_status"
+echo "--------------------------------------------"
+
+exit $max_status
